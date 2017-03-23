42b506068508fd66ede80a2b18703d542c90b587
Michael <michaeldempsey89@gmail.com>
2017-03-23 20:42:38 +0700

    gesture recognition tests

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,6 +7,7 @@ import { PopoverController } from 'ionic-angular'
 import { MenuPopoverPage } from '../../pages/menu-popover/menu-popover';
 import {FormBuilder, Validators,FormGroup, FormControl } from '@angular/forms'
 
+ 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -15,7 +16,10 @@ import {FormBuilder, Validators,FormGroup, FormControl } from '@angular/forms'
 export class HomePage {
 purchaseForm;
   items: FirebaseListObservable<any[]>;
-
+ public press: number = 0;
+  public pan: number = 0;
+  public swipe: number = 0;
+public tap: number = 0;
   constructor(public _navCtrl: NavController, private _angularFire: AngularFire, private _formBuilder: FormBuilder, private _auth: AuthService, private _popoverCtrl: PopoverController) {
   }
 
@@ -34,4 +38,16 @@ ngOnInit() {
     });
   }
 
+  tapEvent(e) {
+    this.tap++
+}
+  pressEvent(e) {
+    this.press++
+  }
+  panEvent(e) {
+    this.pan++
+  }
+  swipeEvent(e) {
+    this.swipe++
+}
 }
